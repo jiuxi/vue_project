94adf42e73f515cc17f829dbcc067c76cb68e179
jiuxi <1095254796@qq.com>
2018-04-16 15:03:23 +0800

    登录页面

---
+++ src/page/login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Header></Header>
+    <div>
+      <ul>
+        <li><input type="text" placeholder="输入密码" v-model="token1"></li>
+        <li>
+          <button @click="btnClick">确定</button>
+        </li>
+      </ul>
+    </div>
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+import Header from '../components/pubHeader'
+import Footer from '../components/pubFooter'
+
+export default {
+  components: {Header, Footer},
+  data () {
+    return {
+      token1: ''
+    }
+  },
+  methods: {
+    btnClick () {
+      if (this.token1 !== '') {
+        localStorage.setItem('token1', this.token1)
+        // 登陆成功进入之前想要进入页面
+        this.$router.push(this.$route.query.redirect || '/')
+      } else {
+        alert('请输入内容')
+      }
+    }
+  }
+}
+</script>
