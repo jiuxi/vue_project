f30a66ce070fce81aae071158286a6dda8c508e9
jiuxi <1095254796@qq.com>
2018-04-17 15:15:22 +0800

    使用vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store/index'
 
 // 引用API文件
 import api from './api/index'
@@ -20,6 +21,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: {App},
   template: '<App/>'
 })
