3d3843e5bed259993142701b276deb1ff70be0e7
jiuxi <1095254796@qq.com>
2018-04-17 15:15:12 +0800

    添加vuex

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    add (state) {
+      state.count += 1
+    },
+    reduce (state) {
+      state.count -= 1
+    }
+  }
+})
+
+export default store
